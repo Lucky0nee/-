11ffb13c61dd7a04aeaba43cc5c70451319b0cc9
Lucky0nee <65708257+Lucky0nee@users.noreply.github.com>
2023-01-03 21:10:12 +0200

    Update and rename README.md to main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+  using namespace std;
+  int main() {
+  double length, width;
+  cout << "Введіть довжину сторони прямокутника: ";
+  cin >> length;
+  cout << "\nВведіть ширину сторони прямокутника: ";
+  cin >> width;
+  cout << "\nS = " << width * length;
+  }
+  /* Напишіть програму для обчислення площі прямокутника. Довжини сторін фігури мають вводитись з клавіатури. */
